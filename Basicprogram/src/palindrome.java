aee9fc1584bb9934592d5eec9091fd0f2ce40cac
Halaswamihs <halaswamyhs74@gmail.com>
2025-01-26 11:17:46 +0530

    palindrome program

---
+++ Basicprogram/src/palindrome.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class palindrome {
+
+	public static void main(String args[])
+	{
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the input");
+		int input=scanner.nextInt();
+		palindromeres(input);
+	}
+	
+	public static void palindromeres(int input)
+	{
+		int copy=input;
+		int result=0;
+		while(input !=0)
+		{
+			int last=input%10;
+			input=input/10;
+			result=result*10+last;
+		}
+		if(copy==result)
+		{
+			System.out.println(copy + " is palindrome");
+		}
+		else {
+			System.out.println(copy+ " is not a palindrome");
+		}
+		
+	}
+}
