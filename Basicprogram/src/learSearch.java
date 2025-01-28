8d32f597c9c52541ed40bdc90b497be6d2ee775b
Halaswamihs <halaswamyhs74@gmail.com>
2025-01-28 19:23:52 +0530

    no message

--- Basicprogram/src/learSearch.java
+++ Basicprogram/src/learSearch.java
@@ -1,8 +1,12 @@
+import java.awt.LinearGradientPaint;
 
 public class learSearch {
 	public static void main(String args[])
 	{
-		
+		int[] arr= {5,9,10,11,15};
+		int key=11;
+		LinearGradientPaint(arr,key);
 	}
+	
 
 }
