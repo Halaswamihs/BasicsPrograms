ef2faf148c11988019ebef192d1425adf0a65f5f
Halaswamihs <halaswamyhs74@gmail.com>
2025-01-28 23:55:43 +0530

    no message

--- Basicprogram/src/PositiveInteger.java
+++ Basicprogram/src/PositiveInteger.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
+//How to find the sum of digits of a positive integer number using recursion ?
 public class PositiveInteger {
 	public static void main(String args[])
 	{
 		Scanner scanner =new Scanner(System.in);
 		System.out.println("enter the numbers");
 		int n=scanner.nextInt();
-		System.out.println(positive(n));
+		if(n<0)
+		{
+			System.out.println("enter only positive number ");
+		}
+		else {
+			System.out.println(positive(n));
+		}
 	}
 	
 		public static int positive(int n)
 		{
 			
-			if(n == 0 )
-			{
-				return 0;
-			}
+			
 			return (n%10) + positive(n/10);
 			
 		}
